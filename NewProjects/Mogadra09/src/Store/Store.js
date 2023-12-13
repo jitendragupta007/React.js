ff0e9e4ce3afdcf97bd5331993119a906ae482de
Jitendra <itsjitendragupta007@gmail.com>
2023-12-13 18:25:42 +0530

    added new content

--- NewProjects/Mogadra09/src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore, createReducer } from "@reduxjs/toolkit";
-import flightReducer from "./FlightResultSlice";
-const store = configureStore({
-    reducer: {
-        flight: flightReducer,
-    },
-});
-
-export default store;
